711f70cea9dd1c1bcb2f12820f3c4bf88ec72929
Lee R. Boynton <lee@boynton.com>
2021-02-14 11:38:26 -0800

    initial implementation, all methods

--- Makefile
+++ Makefile
@@ -20,15 +20,14 @@ $(TARGET)/go.mod:
 	(cd $(TARGET) && go mod init example && go mod tidy)
 
 $(TARGET)/crudl_server.go: $(API)
-	@echo TARGET: $(TARGET)
 	mkdir -p $(TARGET)
 	sadl -g go-server -o $(TARGET) $(API)
 
-$(TARGET)/$(CMD)/main.go:
+$(TARGET)/$(CMD)/main.go: main.go
 	mkdir -p $(TARGET)/$(CMD)
 	cp -p main.go $(TARGET)/$(CMD)/main.go
 
-$(TARGET)/$(CMD)/controller.go:
+$(TARGET)/$(CMD)/controller.go: controller.go
 	mkdir -p $(TARGET)/$(CMD)
 	cp -p controller.go $(TARGET)/$(CMD)/controller.go
 
--- controller.go
+++ controller.go
@@ -26,12 +26,12 @@ func (c *CrudlController) CreateItem(req *crudl.CreateItemRequest) (*crudl.Creat
 	}
 	key := item.Id
 	c.mutex.Lock()
+	defer c.mutex.Unlock()
 	if _, ok := c.storage[key]; ok {
 		return nil, &crudl.BadRequest{Message: fmt.Sprintf("Already exists: %v", key)}
 	}
 	item.Modified = &crudl.Timestamp{Time: time.Now()}
 	c.storage[key] = item
-	c.mutex.Unlock()
 	return &crudl.CreateItemResponse{
 		Item: item,
 	}, nil
@@ -55,14 +55,59 @@ func (c *CrudlController) GetItem(req *crudl.GetItemRequest) (*crudl.GetItemResp
 }
 
 func (c *CrudlController)PutItem(req *crudl.PutItemRequest) (*crudl.PutItemResponse, error) {
-	return nil, nil
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+   if _, ok := c.storage[req.Id]; ok {
+		item := req.Item
+		item.Modified = &crudl.Timestamp{Time: time.Now()}
+		c.storage[req.Id] = item
+		return &crudl.PutItemResponse{
+			Item: item,
+		}, nil
+	}
+	return nil, &crudl.NotFound{Message: fmt.Sprintf("Item not found: %s", req.Id)}
 }
 
 func (c *CrudlController)DeleteItem(req *crudl.DeleteItemRequest) (*crudl.DeleteItemResponse, error) {
-	return nil, nil
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+   if _, ok := c.storage[req.Id]; ok {
+		delete(c.storage, req.Id);
+		return &crudl.DeleteItemResponse{}, nil
+	}
+	return nil, &crudl.NotFound{Message: fmt.Sprintf("Item not found: %s", req.Id)}
 }
 
 func (c *CrudlController)ListItems(req *crudl.ListItemsRequest) (*crudl.ListItemsResponse, error) {
-	return nil, nil
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+	var next crudl.ItemId
+	var count int32
+	limit := req.Limit
+	if limit == 0 { //smithy has no default values, to you get 0 here if the parameter is missing.
+		limit = 10
+	}
+	skip := req.Skip
+	lst := make([]*crudl.Item, 0)
+	for k, v := range c.storage {
+		if skip != "" {
+			if skip != k {
+				continue
+			}
+			skip = ""
+		}
+		count++
+		if count > limit {
+			next = k
+			break
+		}
+		lst = append(lst, v)
+	}
+	return &crudl.ListItemsResponse{
+		Items: &crudl.ItemListing{
+			Items: lst,
+			Next: next,
+		},
+	}, nil
 }
 
